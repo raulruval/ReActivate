e6c75058e9ca3bd7b985fe728db219a0867f81e2
Raúl Ruiz del Valle <raulruizvalle@gmail.com>
2021-07-19 16:40:12 +0200

    #wip add buttons main menu

--- src/objects/custom-button.ts
+++ src/objects/custom-button.ts
@@ -1,39 +1,64 @@
 import Phaser from 'phaser';
 
-export default class CustomBottom extends Phaser.GameObjects.Container
-{
-    private upImage: Phaser.GameObjects.Image;
-    private overImage: Phaser.GameObjects.Image;
+export default class CustomButtom extends Phaser.GameObjects.Container {
+  private upImage: Phaser.GameObjects.Image;
+  private overImage: Phaser.GameObjects.Rectangle;
+  private buttomText: Phaser.GameObjects.Text;
+  private cancelAnimationEmpty: boolean;
+  static barWidth = 333;
 
-    private buttomText: Phaser.GameObjects.Text;
+  constructor(scene: Phaser.Scene, x: number, y: number, upTexture: string, inputText: string) {  
+    super(scene, x, y);
 
-    constructor(scene: Phaser.Scene, x: number, y: number, upTexture: string, overTexture: string, inputText: string){
-        super(scene,x,y);
+    this.upImage = scene.add.image(0, 0, upTexture);
 
-        this.upImage = scene.add.image(0,0,upTexture);
-        this.overImage = scene.add.image(0,0,overTexture);
-        this.buttomText = scene.add.text(0,0,inputText).setOrigin(0.5).setFontSize(45).setFontFamily('Georgia');
+    this.overImage = new Phaser.GameObjects.Rectangle(scene, -166.7, 0, 0, 71, 0x34495e);
+    this.buttomText = scene.add.text(0, 0, inputText).setOrigin(0.5).setFontSize(45).setFontFamily('Georgia');
 
-        this.add(this.upImage);
-        this.add(this.overImage);
-        this.add(this.buttomText);
+    this.add(this.upImage);
+    this.add(this.overImage);
+    this.add(this.buttomText);
 
-        this.overImage.setVisible(false);
+    this.setSize(this.upImage.width, this.upImage.height);
 
-        this.setSize(this.upImage.width,this.upImage.height);
+    this.setInteractive()
+      .on(Phaser.Input.Events.GAMEOBJECT_POINTER_OVER, () => {
+        this.animateToFill(true);
+      })
+      .on(Phaser.Input.Events.GAMEOBJECT_POINTER_OUT, () => {
+        this.animateToEmpty(true);
+      });
+  }
 
-        this.setInteractive()
-        .on(Phaser.Input.Events.GAMEOBJECT_POINTER_OVER, () => {
-            this.upImage.setVisible(false);
-            this.overImage.setVisible(true);
-        })
-        .on(Phaser.Input.Events.GAMEOBJECT_POINTER_OUT, () => {
-            this.upImage.setVisible(true);
-            this.overImage.setVisible(false);
-        })
-        .on('custom', () => {
-            this.upImage.setVisible(false);
-            this.overImage.setVisible(true);
-        })
+  animateToFill(mouseAction: boolean): void {
+    this.cancelAnimationEmpty = true;
+    if (this.overImage.width < CustomButtom.barWidth && !mouseAction) {
+      this.overImage.width = this.overImage.width + 0.02;
+    } else if (mouseAction) {
+      this.overImage.width = CustomButtom.barWidth;
     }
+  }
+  animateToEmpty(mouseActtion: boolean): void {
+    this.cancelAnimationEmpty = false;
+    if (this.overImage.width > 0 && !mouseActtion) {
+      while (this.overImage.width > 0 && !this.cancelAnimationEmpty) {
+        this.overImage.width = this.overImage.width - 0.01;
+      }
+    } else if (mouseActtion) {
+      this.overImage.width = 0;
+    }
+  }
+
+  buttonIsFull(): boolean {
+    if (this.overImage.width >= CustomButtom.barWidth) {
+      this.overImage.width = 0;
+      return true;
+    }
+    return false;
+  }
+
+  getText(): string {
+    return this.buttomText.text;
+  }
+
 }
--- src/scenes/workout-scene.ts
+++ src/scenes/workout-scene.ts
@@ -1,6 +1,6 @@
 import AbstractPoseTrackerScene from '~/pose-tracker-engine/abstract-pose-tracker-scene';
 import Phaser from 'phaser';
-import CustomBottom from '~/objects/custom-button';
+import CustomButtom from '~/objects/custom-button';
 
 export default class WorkoutScene extends AbstractPoseTrackerScene {
   constructor() {
@@ -9,58 +9,82 @@ export default class WorkoutScene extends AbstractPoseTrackerScene {
 
   preload(): void {
     super.preload();
-    this.load.image('button', 'assets/img/button.png');
-    this.load.image('buttonSelected', 'assets/img/buttonSelected.png');
     this.load.image('rec', 'assets/img/rec.png');
-
-    this.load.image('load1', 'assets/img/load1.png');
-    this.load.image('load2', 'assets/img/load2.png');
-    this.load.image('load3', 'assets/img/load3.png');
+    this.load.image('button', 'assets/img/button.png');
   }
 
   private buttonTutorial;
+  private buttonExercise1;
   private handPalm;
+  private buttons: any[] = [];
 
   create(): void {
     super.create();
-    this.buttonTutorial = new CustomBottom(this, 200, 200, 200, 'button', 'buttonSelected', 'Tutorial')
-      .withLeftCap(this.add.image(0, 0, 'load1'))
-      .withMiddleCap(this.add.image(0, 0, 'load2'))
-      .withRightCap(this.add.image(0, 0, 'load3'))
-      .layout();
 
-    this.add.existing(this.buttonTutorial);
+    this.buttonTutorial = new CustomButtom(this, 200, 200, 'button', 'Tutorial');
+    this.buttons.push(this.buttonTutorial);
 
-    // this.buttonTutorial.setInteractive().on(Phaser.Input.Events.GAMEOBJECT_POINTER_DOWN, () => {
-    //   console.log('buttom tutorial');
-    // });
+    this.buttonExercise1 = new CustomButtom(this, 200, 350, 'button', 'Cardio');
+    this.buttons.push(this.buttonExercise1);
+
+    this.buttons.forEach((button) => {
+      this.add.existing(button);
+      this.physics.world.enable(button);
+      button.body.setAllowGravity(false);
+    });
+
     this.handPalm = this.physics.add.sprite(500, 500, 'rec');
     this.add.existing(this.handPalm);
-    this.physics.world.enable(this.buttonTutorial);
-    this.buttonTutorial.body.setAllowGravity(false);
-    this.buttonTutorial.body.moves = false;
   }
 
-  makeSelection() {
-    const fullButton = this.buttonTutorial.progressBar();
-
-    if (fullButton) {
-    console.log('Iniciar');
-    this.buttonTutorial.animateToFill(0.5,500);
-    }
-    
+  makeSelection(selectedButton): any {
+    this.buttons.forEach((button) => {
+      if (button.buttomText.text == selectedButton.buttomText.text) {
+        console.log(button.buttomText.text);
+        button.animateToFill(false);
+        const buttonIsFull = button.buttonIsFull();
+        if (buttonIsFull) {
+          this.scene.start('hello-world-scene');
+        }
+      }
+    });
   }
 
-  updateHand(coords) {
+  moveHand(coords) {
     if (this.handPalm && coords) {
-      //console.log('actualiza coords' + coords.x * 1000 + ' y ' + coords.y * 1000);
       this.physics.moveTo(this.handPalm, coords.x * 1280, coords.y * 720, 800);
     }
   }
 
   update(time: number, delta: number): void {
-    this.physics.add.overlap(this.buttonTutorial, this.handPalm, this.makeSelection, undefined, this);
-    //this.physics.collide(this.handPalm,this.buttonTutorial,this.makeSelection());
+    this.buttons.forEach((button) => {
+      this.physics.add.overlap(
+        button,
+        this.handPalm,
+        function (this) {
+          button.animateToFill(false);
+           const buttonIsFull: CustomButtom = button.buttonIsFull();
+          if (buttonIsFull) {
+            switch (button.getText()) {
+              case 'Tutorial':
+                this.scene.start('hello-world-scene');
+                break;
+              case 'Cardio':
+                console.log('Cardio');
+                break;
+
+              default:
+                break;
+            }
+          }
+        },
+        undefined,
+        this,
+      );
+      if (this.handPalm.body.touching.none) {
+        button.animateToEmpty(false);
+      }
+    });
 
     super.update(time, delta, {
       renderElementsSettings: {
@@ -68,13 +92,11 @@ export default class WorkoutScene extends AbstractPoseTrackerScene {
         shouldDrawPoseLandmarks: true,
       },
       beforePaint: (poseTrackerResults, canvasTexture) => {
-        
+        this.moveHand(poseTrackerResults.poseLandmarks ? poseTrackerResults.poseLandmarks[20] : undefined);
         // This function will be called before refreshing the canvas texture.
         // Anything you add to the canvas texture will be rendered.
       },
       afterPaint: (poseTrackerResults) => {
-        this.updateHand(poseTrackerResults.poseLandmarks ? poseTrackerResults.poseLandmarks[20] : undefined);
-
         // This function will be called after refreshing the canvas texture.
       },
     });
